900abdbd8752b3a6ddeaf21e2c88258fea26df4b
Franco N. Colaizzi <fnc314@gmail.com>
2025-07-21 15:25:19 -0400

    new site - nav fix1

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 import "@material/web";
 import "./partials/info/info-partial";
-
-/**
- * Determines which {@link MdTabs} is selected
- * @param tabs The {@link MdTabs} from {@link document.getElementById}
- * @return The {@link MdTab} selected *or* {@link nul}
- */
-function getVisibleTab(tabs) {
-  const activeTab = tabs.activeTab?.getAttribute("aria-controls");
-  return tabs.getRootNode().querySelector(`#${activeTab}`);
-}
--- partials/nav/nav-partial.js
+++ partials/nav/nav-partial.js
@@ -26,6 +26,9 @@ export class NavPartial extends LitElement {
     return {};
   }
 
+  /**
+   * The current {@link MdPrimaryTab} which is actively being displayed
+   */
   #activeTab = null;
 
   /**
@@ -34,23 +37,33 @@ export class NavPartial extends LitElement {
    * @return The {@link MdTab} selected *or* {@link null}
    */
   #getVisibleTab(tabs) {
-    const activeTab = tabs.activeTab?.getAttribute("aria-controls");
-    return tabs.getRootNode().querySelector(`#${activeTab}`);
+    const activeTabControl = tabs.children.item(tabs.activeTabIndex).getAttribute("aria-controls");
+    return document.querySelector(`#${activeTabControl}`);
   }
 
-  constructor() {
-    super();
+  /**
+   * Queries {@link this.shadowRoot} for {@link #nav-md-tabs} returning the {@link NodeCollection}
+   *   or {@link null}
+   * @return A {@link NodeCollection} or {@link null}
+   */
+  #getTabs() {
+    return this.shadowRoot.querySelector("#nav-md-tabs") ?? null;
   }
 
-  connectedCallback() {
-    super();
-    const tabs = this.querySelector("#nav-md-tabs") ?? null;
+  /**
+   * A lifecycle-esque callback offered by {@link LitElement} implementations allowing a post-render
+   *   hook during which event listeners can be wired appropriately
+   * @param {*} _changedProperties A framework-provided {@link Object} with meta-data relevant to the
+   *   ongoing render cycle.  Currently unused
+   */
+  async firstUpdated(_changedProperties) {
+    await new Promise((r) => setTimeout(r, 0));
+    const tabs = this.#getTabs();
     if (tabs == null) {
       return;
     }
     this.#activeTab = this.#getVisibleTab(tabs);
-
-    tabs.addEventListener("change", () => {
+    tabs.addEventListener("change", e => {
       if (this.#activeTab) {
         this.#activeTab.hidden = true;
       }
@@ -59,6 +72,10 @@ export class NavPartial extends LitElement {
         this.#activeTab.hidden = false
       }
     });
+  }
+
+  constructor() {
+    super();
   }
 
   render() {
