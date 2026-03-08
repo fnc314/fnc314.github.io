ba3001945749a7d9d98b18d9faa28c175c95611d
Franco N. Colaizzi <fnc314@gmail.com>
2026-03-07 20:50:48 -0500

    manifest.json fixes

--- rollup.config.js
+++ rollup.config.js
@@ -39,7 +39,7 @@ export default {
       ]
     }),
     copy({
-      patterns: [ "assets/**/*.jpg" ], rootDir: "./",
+      patterns: [ "assets/**/*.{jpg,svg}" ], rootDir: "./",
     }),
     copy({
       patterns: [ "files/**/*.pdf" ], rootDir: "./assets",
