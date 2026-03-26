a7aab33ee5a97fe4b78b59e88fe9a9aac4166fa6
Franco N. Colaizzi <fnc314@gmail.com>
2026-03-26 01:18:24 -0400

    custom-elements.json fixes

--- src/components/dialogs/step-up/step-up-dialog.ts
+++ src/components/dialogs/step-up/step-up-dialog.ts
@@ -4,17 +4,20 @@ import "@material/web/button/outlined-button";
 import "@material/web/dialog/dialog";
 import { MdDialog } from "@material/web/dialog/dialog";
 import "@material/web/icon/icon";
-import { css, html, LitElement, TemplateResult } from "lit-element";
+import { LitElement, TemplateResult, css, html } from "lit-element";
 import { classMap } from "lit-html/directives/class-map.js";
 import { customElement, property, query } from "lit/decorators.js";
 
-export type ConfirmDialogStyle = "confirm" | "warning" | "attention"
+export type ConfirmDialogStyle = "confirm" | "warning" | "attention";
 
 /**
  * A versatile confirmation dialog used to verify user intent before performing
  * significant actions like resetting settings.
  *
  * @element step-up-dialog
+ * @slot headline - The {@link MdDialog} headline {@link slot}
+ * @slot content - The body of the {@link MdDialog}
+ * @slot actions - The area of dialog action buttons
  * @fires stepUpComplete - Dispatched when the user confirms or cancels the action.
  */
 @customElement("step-up-dialog")
@@ -23,7 +26,6 @@ export class StepUpDialog extends LitElement {
     MaterialTypescaleStyles,
     css`
       :host {
-
       }
 
       md-dialog {
@@ -77,9 +79,8 @@ export class StepUpDialog extends LitElement {
           --md-outlined-button-hover-label-text-color: var(--md-sys-color-on-error);
           --md-outlined-button-focus-outline-color: var(--md-sys-color-on-error);
         }
-
       }
-    `
+    `,
   ];
 
   /**
@@ -117,29 +118,29 @@ export class StepUpDialog extends LitElement {
         detail: {
           cancelled: isCancel,
           confirmed: !isCancel,
-        }
-      })
+        },
+      }),
     );
     await this._mdDialog.close();
   }
-
-  private headlines: Record<ConfirmDialogStyle, TemplateResult> = {
-    confirm: html`<h2 class="md-typescale-headline-small">Please Confirm</h2>`,
-    warning: html`<h2 class="md-typescale-headline-small">Warning</h2>`,
-    attention: html`<h2 class="md-typescale-headline-small">Attention!</h2>`,
-  };
 
   private icons: Record<ConfirmDialogStyle, TemplateResult> = {
     confirm: html`<md-icon slot="icon">check_circle</md-icon>`,
     warning: html`<md-icon slot="icon">warning</md-icon>`,
     attention: html`<md-icon slot="icon">report</md-icon>`,
-  }
+  };
 
   private primaryActions: Record<ConfirmDialogStyle, TemplateResult> = {
-    confirm: html`<md-filled-button @click=${(event: PointerEvent) => this.onButtonClick(false, event)}>Confirm</md-filled-button>`,
-    warning: html`<md-filled-button @click=${(event: PointerEvent) => this.onButtonClick(false, event)}>Continue</md-filled-button>`,
-    attention: html`<md-filled-button @click=${(event: PointerEvent) => this.onButtonClick(false, event)}>Accept</md-filled-button>`,
-  }
+    confirm: html`
+      <md-filled-button @click=${(event: PointerEvent) => this.onButtonClick(false, event)}>Confirm</md-filled-button>
+    `,
+    warning: html`
+      <md-filled-button @click=${(event: PointerEvent) => this.onButtonClick(false, event)}>Continue</md-filled-button>
+    `,
+    attention: html`
+      <md-filled-button @click=${(event: PointerEvent) => this.onButtonClick(false, event)}>Accept</md-filled-button>
+    `,
+  };
 
   override render() {
     const mdDialogClasses = {
@@ -147,21 +148,41 @@ export class StepUpDialog extends LitElement {
       attention: this.dialogStyle === "attention",
       warning: this.dialogStyle === "warning",
     };
+    let defaultHeadlineSlotText: string;
+    switch (this.dialogStyle) {
+      case "confirm":
+        defaultHeadlineSlotText = "Please Confirm";
+        break;
+      case "warning":
+        defaultHeadlineSlotText = "Warning";
+        break;
+      case "attention":
+        defaultHeadlineSlotText = "Attention!";
+        break;
+    }
+
+    const defaultHeadlineSlotContent: TemplateResult = html`
+      <h2 class="md-typescale-headline-small">${defaultHeadlineSlotText}</h2>
+    `;
+
     return html`
-      <md-dialog id="step-up-dialog" type="alert" class=${classMap(mdDialogClasses)}>
+      <md-dialog
+        id="step-up-dialog"
+        type="alert"
+        class=${classMap(mdDialogClasses)}
+      >
         ${this.icons[this.dialogStyle]}
-        <div slot="headline">
-          ${this.headlines[this.dialogStyle]}
-        </div>
+        <div slot="headline">${defaultHeadlineSlotContent}</div>
         <div slot="content">
-          <form method="dialog" id=${`dialog-${this.dialogStyle}`}>
+          <form
+            method="dialog"
+            id=${`dialog-${this.dialogStyle}`}
+          >
             <p>${this.dialogContentString}</p>
           </form>
         </div>
         <div slot="actions">
-          <md-outlined-button
-            @click=${(event: PointerEvent) => this.onButtonClick(true, event)}
-            >
+          <md-outlined-button @click=${(event: PointerEvent) => this.onButtonClick(true, event)}>
             Cancel
           </md-outlined-button>
           ${this.primaryActions[this.dialogStyle]}
@@ -169,10 +190,10 @@ export class StepUpDialog extends LitElement {
       </md-dialog>
     `;
   }
-};
+}
 
 export type OpenStepUpDialog = CustomEvent<object>;
-export type CompleteStepUpDialog = CustomEvent<{ cancelled: boolean, confirmed: boolean }>;
+export type CompleteStepUpDialog = CustomEvent<{ cancelled: boolean; confirmed: boolean }>;
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -180,8 +201,8 @@ declare global {
   }
 
   interface GlobalEventHandlersEventMap {
-    "stepUpOpen": OpenStepUpDialog;
+    stepUpOpen: OpenStepUpDialog;
 
-    "stepUpComplete": CompleteStepUpDialog;
+    stepUpComplete: CompleteStepUpDialog;
   }
-};
+}
