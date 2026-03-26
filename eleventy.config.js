55fad5d44c473ffcfbd406b5de18eae964babdc1
Franco N. Colaizzi <fnc314@gmail.com>
2026-03-26 15:32:52 -0400

    adds to eleventy cofig

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,38 @@
+import { IdAttributePlugin } from "@11ty/eleventy";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+
 /** @param {import("@11ty/eleventy/UserConfig").default} eleventyConfig */
 export default function (eleventyConfig) {
   eleventyConfig.setQuietMode(false);
+  eleventyConfig.addPlugin(IdAttributePlugin, {
+    checkDuplicates: "error",
+  });
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    alwaysWrapLineHighlights: false,
+    errorOnInvalidLanguage: true,
+    languages: ["ts"],
+    preAttributes: {
+      "data-language": ({ language }) => language,
+    },
+    codeAttributes: {},
+  });
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    extensions: ["ts"],
+
+    // output image formats
+    formats: ["avif", "webp", "jpeg"],
+
+    // output image widths
+    widths: ["auto"],
+
+    // optional, attributes assigned on <img> nodes override these values
+    htmlOptions: {
+      imgAttributes: {
+        loading: "lazy",
+        decoding: "async",
+      },
+      pictureAttributes: {},
+    },
+  });
 }
