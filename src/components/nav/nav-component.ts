f79d8d0c6fdfd3ad5625d2bf027e65a6f58f211b
Franco N. Colaizzi <fnc314@gmail.com>
2026-04-01 19:55:33 -0400

    suppresses blog page

--- src/components/nav/nav-component.ts
+++ src/components/nav/nav-component.ts
@@ -137,7 +137,7 @@ export class NavComponent extends LitElement {
     work: createRef(),
     code: createRef(),
     profile: createRef(),
-    blog: createRef(),
+    // blog: createRef(),
   };
 
   #routes: Route[] = Object.values(Routes);
@@ -306,15 +306,15 @@ export class NavComponent extends LitElement {
         </md-icon>
         Code
       `,
-      blog: html`
-        <md-icon
-          slot="icon"
-          filled=${this._activeRoute === Routes.BLOG || this._exitingRoute === Routes.BLOG}
-        >
-          newsmode
-        </md-icon>
-        Blog
-      `,
+      // blog: html`
+      //   <md-icon
+      //     slot="icon"
+      //     filled=${this._activeRoute === Routes.BLOG || this._exitingRoute === Routes.BLOG}
+      //   >
+      //     newsmode
+      //   </md-icon>
+      //   Blog
+      // `,
     };
 
     const tabs: TemplateResult[] = this.#routes.map(
