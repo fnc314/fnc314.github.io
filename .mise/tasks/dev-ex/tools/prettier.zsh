f8cd3a7b4e2b1987e7f6bcc59a5d1c53a4fc3e08
Franco N. Colaizzi <fnc314@gmail.com>
2026-06-08 15:22:32 -0400

    prettier.zsh more robust

--- .mise/tasks/dev-ex/tools/prettier.zsh
+++ .mise/tasks/dev-ex/tools/prettier.zsh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 #MISE description="Invokes `pnpm prettier` with `./.config/prettier/prettier.config.mts"
 #MISE alias="dx:t:prettier"
-#USAGE flag "--fix" help="Passes `--write` to `prettier` to fix files" default="false"
 #USAGE arg "<logLevel>" help="Determines `--log-level` passed to `prettier`" default="warn" {
 #USAGE   choices "warn" "info" "debug"
 #USAGE }
+#USAGE arg "<mode>" help="Passes `--write`` or `--check`` to `prettier`" default="check" {
+#USAGE   choices "check" "write"
+#USAGE }
 
-declare PRETTIER_FIX="${usage_fix:=false}"
 typeset -a PRETTIER_FLAGS
 PRETTIER_FLAGS=(
   --ignore-path ./.config/prettier/.prettierignore
@@ -15,9 +16,14 @@ PRETTIER_FLAGS=(
   --log-level ${usage_log_level:=warn}
 )
 
-if [[ "$PRETTIER_FIX" == "true" ]]; then
+declare PRETTIER_MODE="${usage_mode:=check}"
+if [[ "$PRETTIER_MODE" == "write" ]]; then
   PRETTIER_FLAGS+=(
     --write
+  )
+elif [[ "$PRETTIER_MODE" == "check" ]]; then
+  PRETTIER_FLAGS+=(
+    --check
   )
 else
   PRETTIER_FLAGS+=(
@@ -27,4 +33,4 @@ fi
 
 print -f "${PRETTIER_FLAGS}\n\n"
 
-NODE_OPTIONS="--experimental-strip-types" pnpm prettier "./src/**/*.{css,html,js,json,ts}" "${PRETTIER_FLAGS[@]}"
+NODE_OPTIONS="--experimental-strip-types" pnpm prettier "./src/**/*.{css,json,ts}" "${PRETTIER_FLAGS[@]}"
