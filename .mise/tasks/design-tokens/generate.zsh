eb88071ea2c6a0032ae912c927c29d2c65dd2fbd
Franco N. Colaizzi <fnc314@gmail.com>
2026-06-08 08:19:50 -0400

    mise.toml -> .config/mise/mise.toml

---
+++ .mise/tasks/design-tokens/generate.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env/ zsh
+#MISE description="Generate design tokens using Style Dictionary"
+#MISE dir="{{cwd}}/design-tokens"
+#MISE alias="dt:g"
+
+echo "Cleaning design tokens for @fnc314/design-tokens..."
+pnpm style-dictionary clean -v -c ./.config/style-dictionary/config.ts
+echo "Generating design tokens for @fnc314/design-tokens..."
+pnpm style-dictionary build -v -c ./.config/style-dictionary/config.ts
+echo "Design tokens generated successfully."
