fd64919cfc553587e026975d66671f1d02c91a3b
Franco N. Colaizzi <fnc314@gmail.com>
2026-06-08 21:23:12 -0400

    drops VSCode extension; fixes reference to `.config/vite/vite.config.ts`

--- .mise/tasks/dev-ex/tools/style-dictionary.zsh
+++ .mise/tasks/dev-ex/tools/style-dictionary.zsh
@@ -2,8 +2,8 @@
 #MISE description="Invokes `style-dictionary` with `./.config/style-dictionary/config.ts`"
 #MISE alias="dx:t:style-dictionary"
 
-echo "Cleaning design tokens for @fnc314/design-tokens..."
+print -f "Cleaning design tokens for @fnc314/design-tokens..."
 pnpm style-dictionary clean -v -c ./.config/style-dictionary/config.ts
-echo "Generating design tokens for @fnc314/design-tokens..."
+print -f "Generating design tokens for @fnc314/design-tokens..."
 pnpm style-dictionary build -v -c ./.config/style-dictionary/config.ts
-echo "Design tokens generated successfully."
+print -f "Design tokens generated successfully."
