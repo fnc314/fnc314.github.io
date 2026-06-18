620e51ac21e75dab893d1a27a56cd1029bf940c6
Franco N. Colaizzi <fnc314@gmail.com>
2026-06-18 14:53:40 -0400

    separate calls for material symbols

--- src/components/code/repo/code-repo.ts
+++ src/components/code/repo/code-repo.ts
@@ -1,9 +1,9 @@
 import { CodeRepoStyles } from "@/components/code/repo/code-repo.styles";
-import { type CodeRepoData } from "@/components/code/repo/code-repo.types";
+import { type CodeRepoData, type CodeRepoTech } from "@/components/code/repo/code-repo.types";
 import { UIAwareElement } from "@/mixins/ui-aware-element/ui-aware-element";
 import { TextStyles } from "@/styles/text";
 import { Breakpoints, readCSSProperty } from "@fnc314/design-tokens";
-import { html, nothing, unsafeCSS } from "lit";
+import { type TemplateResult, html, nothing, unsafeCSS } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 /**
@@ -27,6 +27,70 @@ export class CodeRepo extends UIAwareElement {
     TextStyles,
     CodeRepoStyles,
   ];
+
+  private createWordTagLI(tech: CodeRepoTech): TemplateResult {
+    const imgSrc =
+      typeof tech.designToken === "string"
+        ? readCSSProperty(tech.designToken)
+        : readCSSProperty(
+            this.darkMode
+              ? tech.designToken.dark
+              : tech.designToken.light
+          );
+
+    const tagId: string = `${
+      typeof tech.designToken === "string"
+        ? tech.designToken
+        : this.darkMode
+          ? tech.designToken.dark
+          : tech.designToken.light
+    }-word-tag`;
+
+    const imgTag = imgSrc
+      ? html`
+          <img
+            loading="lazy"
+            role="img"
+            aria-describedby="${tagId}"
+            src="${imgSrc}"
+            alt="${tech.name}"
+            slot="icon"
+            title="${tech.name}"
+          />
+        `
+      : nothing;
+
+    const variant = imgTag === nothing
+      ? "text-only"
+      : (
+        this.touchScreen || this.breakpoint === Breakpoints.BreakpointLabels.mobile
+        ? "icon-text"
+        : "icon-only"
+      );
+
+    console.warn(
+      `
+      CODE REPO
+      TouchScreen ${this.touchScreen}
+      Breakpoint ${this.breakpoint}
+      Variant ${variant}
+      Tag ID ${tagId}
+      Tech ${JSON.stringify(tech, null, 2)}
+      `
+    );
+    return html`
+      <li>
+        <word-tag
+          id="${tagId}"
+          .hrefUrl=${tech.url}
+          .word=${tech.name}
+          .variant=${variant}
+        >
+          ${imgTag}
+        </word-tag>
+      </li>
+    `;
+  }
 
   override render() {
     const token = this.darkMode
@@ -62,59 +126,7 @@ export class CodeRepo extends UIAwareElement {
 
         <footer>
           <ul aria-label="${"Technologies used"}">
-            ${this.codeRepo.tech.map((tech) => {
-              const imgSrc =
-                typeof tech.designToken === "string"
-                  ? readCSSProperty(tech.designToken)
-                  : readCSSProperty(
-                      this.darkMode
-                        ? tech.designToken.dark
-                        : tech.designToken.light
-                    );
-
-              const tagId: string = `${
-                typeof tech.designToken === "string"
-                  ? tech.designToken
-                  : this.darkMode
-                    ? tech.designToken.dark
-                    : tech.designToken.light
-              }-word-tag`;
-
-              const imgTag = imgSrc
-                ? html`
-                    <img
-                      loading="lazy"
-                      role="img"
-                      aria-describedby="${tagId}"
-                      src="${imgSrc}"
-                      alt="${tech.name}"
-                      slot="icon"
-                      title="${tech.name}"
-                    />
-                  `
-                : nothing;
-
-              const variant = imgTag === nothing
-                ? "text-only"
-                : (
-                  this.breakpoint === Breakpoints.BreakpointLabels.mobile
-                  ? "icon-text"
-                  : "icon-only"
-                );
-
-              return html`
-                <li>
-                  <word-tag
-                    id="${tagId}"
-                    .hrefUrl=${tech.url}
-                    .word=${tech.name}
-                    .variant=${variant}
-                  >
-                    ${imgTag}
-                  </word-tag>
-                </li>
-              `;
-            })}
+            ${this.codeRepo.tech.map((tech) => this.createWordTagLI(tech))}
           </ul>
         </footer>
       </article>
