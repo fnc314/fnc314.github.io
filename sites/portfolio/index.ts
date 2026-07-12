f39a60e60ed2de7d0026f38df417897dd0a0e3b0
Franco N. Colaizzi <fnc314@gmail.com>
2026-07-12 16:42:17 -0400

    new packages

---
+++ sites/portfolio/index.ts
@@ -0,0 +1,72 @@
+import "@/index.css";
+import "@fnc314/packages.components";
+import "@fnc314/packages.data";
+import "@fnc314/packages.design-tokens";
+import "@fnc314/packages.services";
+import { MaterialCSSStyleSheet, colorSchemeConfigsToMaterialSchemeName, configsService, onThemeChange, themeService } from "@fnc314/packages.services";
+import "@fnc314/packages.types";
+import { type ColorSchemeConfigChange, type ColorSchemeConfigs } from "@fnc314/packages.types";
+import { styles as typescaleStyles } from "@material/web/typography/md-typescale-styles.js";
+import "material-symbols/outlined.css";
+import "material-symbols/sharp.css";
+// import "prop-for-that/auto";
+
+/**
+ * Modifies a particular `<meta>` tag in the DOM
+ *
+ * @param color - A `#`-prefixed `string`
+ */
+const setMetaThemeColor: (color: `#${string}`) => void = (color: `#${string}`) =>
+  document.getElementById("meta-theme-color")?.setAttribute("content", color);
+
+/**
+ * A listener for {@link ColorSchemeConfiChange} events
+ *
+ * @param event - The particular `event`
+ */
+const onColorSchemeChange = (event: ColorSchemeConfigChange) => {
+  const customEvent: ColorSchemeConfigChange = event as ColorSchemeConfigChange;
+  if (!document.startViewTransition) {
+    applyColorSchemeConfigs(customEvent.detail)
+  } else {
+    document.startViewTransition(() => {
+      applyColorSchemeConfigs(customEvent.detail)
+    });
+  }
+};
+
+/**
+ * Applys the provided `configs`
+ *
+ * @param configs - Particular {@link ColorSchemeConfigs} to apply
+ */
+const applyColorSchemeConfigs: (configs: ColorSchemeConfigs) => void = (configs: ColorSchemeConfigs) => {
+  const matScheme = themeService.currentThemeConfig().materialSchemes[
+    colorSchemeConfigsToMaterialSchemeName(configs)
+  ];
+
+  MaterialCSSStyleSheet.replaceSync(matScheme.cssText);
+  setMetaThemeColor(themeService.themeJson().primary);
+}
+
+/** Bootstrapping listener for `DOMContentLoaded` */
+const domLoadedListener = () => {
+  document.removeEventListener("DOMContentLoaded", domLoadedListener);
+
+  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", onThemeChange);
+
+  if (typescaleStyles.styleSheet) {
+    document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
+  }
+
+  document.adoptedStyleSheets.push(MaterialCSSStyleSheet);
+
+  applyColorSchemeConfigs(
+    configsService.loadConfigs().colorScheme
+  );
+
+  // Migrated from AppShell
+  document.addEventListener("color_scheme.change", onColorSchemeChange);
+};
+
+document.addEventListener("DOMContentLoaded", domLoadedListener);
